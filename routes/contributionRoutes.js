93c653b2619a06ccece40f30d38d39b298449674
Ridhwan bakare <ridmartrite@gmail.com>
2024-07-23 12:35:44 +0100

    auth

--- routes/contributionRoutes.js
+++ routes/contributionRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const contributionController = require('../controllers/contributionController');
-const auth = require('../middlewares/auth');  // Assuming you have authentication middleware
+const auth = require('../middlewares/auth');
 
 router.post('/contribute', auth, contributionController.addContribution);
 
